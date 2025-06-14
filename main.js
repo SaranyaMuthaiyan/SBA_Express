20f9a5a847d0cd07b87bd09487b31ed88c577a0c
Saranya Muthaiyan <saramuthaiyan5@gmail.com>
2025-06-14 15:17:00 -0400

    corrections

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ const port = 8080
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(log);
+app.use(errorHandler);
+app.use(router)
 
 
 app.set("view engine", "ejs");
